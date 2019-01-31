fd6c42d0724e07889d90a7488a165dfb32995554
d-das <jdawsonweehunt@gmail.com>
2019-01-31 12:14:39 -0600

    added components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { CodeComponent } from './code/code.component';
+import { WritingComponent } from './writing/writing.component';
+import { PhotoComponent } from './photo/photo.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "code", component: CodeComponent },
+  { path: "writing", component: WritingComponent },
+  { path: "photo", component: PhotoComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+/*
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PokemonListComponent } from "../pokemon-list/pokemon-list.component";
+
+const appRoutes: Routes = [
+  {
+    path: "pokemons",
+    component: PokemonListComponent
+  }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CodeComponent } from './code/code.component';
+import { WritingComponent } from './writing/writing.component';
+import { PhotoComponent } from './photo/photo.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodeComponent,
+    WritingComponent,
+    PhotoComponent,
+    ContactComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
