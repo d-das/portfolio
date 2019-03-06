b04a371e0f79341db63662d7f02adfde4487b2d4
d-das <jdawsonweehunt@gmail.com>
2019-03-05 19:22:35 -0600

    updated civ.

---
+++ src/app/civilization/civilization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-civilization',
+  templateUrl: './civilization.component.html',
+  styleUrls: ['./civilization.component.scss']
+})
+export class CivilizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
